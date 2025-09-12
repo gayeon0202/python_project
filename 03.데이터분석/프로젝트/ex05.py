3049367032d3833edde160401c4f82b185b39802
gayeon0202 <369369n@naver.com>
2025-09-12 18:15:13 +0900

    09-12

--- 03.데이터분석/프로젝트/ex05.py
+++ 03.데이터분석/프로젝트/ex05.py
@@ -56,47 +56,46 @@ def submenu():
             while True:
                 sel = inputNum('1.최신입력순|2.이름순|3.성적순>')
                 if sel=='': break
-                elif sel ==1: df=df.sort_index(ascending=False)
-                elif sel ==2: df=df.sort_values('이름')
+                elif sel==1: df=df.sort_index(ascending=False)
+                elif sel==2: df=df.sort_values('이름')
                 elif sel==3: df=df.sort_values('평균', ascending=False)
-                for idx in df.head().index:
+                for idx in df.head(3).index:
                     row = df.loc[idx]
                     print(f'지원번호:{idx:02d}', end=' ')
-                    print(f'이름:{row["이름"]}', end =' ')
+                    print(f'이름:{row["이름"]}', end=' ')
                     print(f'학교:{row["학교"]}')
                     for col in cols:
                         print(f'{col}:{row[col]:.0f}', end=' ')
                     print(f'평균:{row["평균"]:.2f}')
                     print('-' * 60)
             input('아무키나 누르세요!')
-        elif menu == '3':  #검색
+        elif menu=='3':#검색
             while True:
-                sel = inputNum('1:지원번호|2:학교|3.이름> ')
-                if sel == '':break
-                elif sel == 1:
-                    no = inputNum('지원번호> ')
+                sel = inputNum('1.지원번호|2.학교|3.이름>')
+                if sel=='':
+                    break
+                elif sel==1:
+                    no = inputNum('지원번호>')
                     if not no in df.index:
                         print('해당 지원번호가 없습니다.')
                     else:
-                        row=df.loc[no]
-                        print(f"지원번호: {no}  이름: {row['이름']}  학교: {row['학교']}  평균: {row['평균']:.2f}")
-
-                elif sel == 2:
-                    word = input('검색어> ')
+                        row = df.loc[no]
+                        print(f'지원번호:{no} 이름:{row["이름"]} 학교:{row["학교"]} 평균:{row["평균"]:.2f}')
+                elif sel==2:
+                    word = input('검색어>')
                     filt = df['학교'].str.contains(word)
                     if len(df[filt].index)==0:
                         print('검색내용이 없습니다.')
                         continue
                     for idx in df[filt].index:
                         row = df.loc[idx]
-                        print(f"지원번호: {idx}  이름: {row['이름']}  학교: {row['학교']}  평균: {row['평균']:.2f}")
-
-                elif sel == 3:
-                    word = input('검색어> ')
+                        print(f'지원번호:{idx} 이름:{row["이름"]} 학교:{row["학교"]} 평균:{row["평균"]:.2f}')
+                elif sel==3:
+                    word = input('검색어>')
                     filt = df['이름'].str.contains(word)
                     for idx in df[filt].index:
                         row = df.loc[idx]
-                        print(f"지원번호: {idx}  이름: {row['이름']}  학교: {row['학교']}  평균: {row['평균']:.2f}")
+                        print(f'지원번호:{idx} 이름:{row["이름"]} 학교:{row["학교"]} 평균:{row["평균"]:.2f}')
 
             input('아무키나 누르세요!')
         elif menu=='4':#삭제
